905a5f75c5a5352366a93fe63d67893813a4455c
Tom Humphreys <Tom01098Code@gmail.com>
2023-01-01 22:19:30 +0000

    Add an opening quickstart with an example. Getting the example to compile was an absolute nightmare. Happy New Year!

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,54 @@
+//! Cache AWS Secrets Manager secrets in your AWS Lambda function, reducing latency (we don't need to query another service) and cost ([Secrets Manager charges based on queries]).
+//! 
+//! # Quickstart
+//! Add the [AWS Parameters and Secrets Lambda Extension] [layer to your Lambda function]. Only version 2 of this layer is currently supported.
+//! 
+//! Assuming a secret exists with the name "backend-server" containing a key/value pair with a key of "api_key" and a value of 
+//! "dd96eeda-16d3-4c86-975f-4986e603ec8c" (our super secret API key to our backend), this code will get the secret from the cache, querying
+//! Secrets Manager if it is not in the cache, and present it in a strongly-typed `BackendServer` object. 
+//! 
+//! ```rust
+//! use aws_parameters_and_secrets_lambda::Manager;
+//! use serde::Deserialize;
+//! 
+//! #[derive(Deserialize)]
+//! struct BackendServer {
+//!     api_key: String
+//! }
+//! 
+//! # let server = httpmock::MockServer::start();
+//! # let mock = server.mock(|when, then| {
+//! #     when.method("GET").path("/secretsmanager/get");
+//! #     then.status(200).body("{\"SecretString\": \"{\\\"api_key\\\": \\\"dd96eeda-16d3-4c86-975f-4986e603ec8c\\\"}\"}");
+//! # });
+//! # 
+//! # temp_env::with_vars(
+//! #     vec![
+//! #         ("AWS_SESSION_TOKEN", Some("xyz")),
+//! #         ("PARAMETERS_SECRETS_EXTENSION_HTTP_PORT", Some(&server.port().to_string()))
+//! #     ],
+//! #     || {
+//! #         tokio_test::block_on(
+//! #             std::panic::AssertUnwindSafe(
+//! #                 async {
+//! let manager = Manager::default();
+//! let secret = manager.get_secret("backend-server");
+//! let secret_value: BackendServer = secret.get_typed().await?;
+//! assert_eq!("dd96eeda-16d3-4c86-975f-4986e603ec8c", secret_value.api_key);
+//! #                     Ok::<_, anyhow::Error>(())
+//! #                 }
+//! #             )
+//! #         );
+//! #     }
+//! # );
+//! #
+//! # mock.assert();
+//! ```
+//! 
+//! [Secrets Manager charges based on queries]: https://aws.amazon.com/secrets-manager/pricing/
+//! [AWS Parameters and Secrets Lambda Extension]: https://docs.aws.amazon.com/secretsmanager/latest/userguide/retrieving-secrets_lambda.html
+//! [layer to your Lambda function]: https://docs.aws.amazon.com/lambda/latest/dg/invocation-layers.html
+
 use std::fmt::Debug;
 use std::{env, sync::Arc};
 
